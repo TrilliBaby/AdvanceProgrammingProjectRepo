9cd996065aa64976d2a51db59e72838eba5e8fcb
Gabrielle Flash <146474677+Gabi-728@users.noreply.github.com>
2025-02-11 02:31:25 -0500

    Update Customer.java

--- AdvanceProgrammingProject/src/javaEntertainment/Customer.java
+++ AdvanceProgrammingProject/src/javaEntertainment/Customer.java
@@ -1,7 +1,10 @@
 package javaEntertainment;
 
-public class Customer extends Person{
-	
+import java.io.Serializable;
+
+public class Customer extends Person implements Serializable{
+
+	private static final long serialVersionUID =1L;
 	private String cusID;
 	private String phoneNumber;
 	private char gender;
