95cb2e65b9177f9058e1ae74b6b9e0d6b7f8bdc8
Gabrielle Flash <146474677+Gabi-728@users.noreply.github.com>
2025-02-11 02:42:35 -0500

    Event.java

--- AdvanceProgrammingProject/src/javaEntertainment/Event.java
+++ AdvanceProgrammingProject/src/javaEntertainment/Event.java
@@ -1,7 +1,10 @@
 package javaEntertainment;
 
-public class Event {
-	
+import java.io.Serializable;
+
+public class Event implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	private String eID;  
 	private String name;
 	private String address; 
