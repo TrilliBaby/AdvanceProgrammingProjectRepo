57df8ae623af1552e87cd8b669c0345210735485
TrilliBaby <bri.mowatt@gmail.com>
2025-03-18 07:39:36 -0500

    Update to Registration Page

--- AdvanceProgrammingProject/src/Gui/RegistrationView.java
+++ AdvanceProgrammingProject/src/Gui/RegistrationView.java
@@ -20,6 +20,7 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import com.toedter.calendar.JDateChooser;
 
 public class RegistrationView extends JFrame {
 
@@ -28,7 +29,6 @@ public class RegistrationView extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
-	private JTextField textField_4;
 	private JTextField textField_5;
 
 	/**
@@ -57,19 +57,19 @@ public class RegistrationView extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Home");
 		menuBar.add(mntmNewMenuItem_4);
 		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Contact Us");
 		menuBar.add(mntmNewMenuItem_3);
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("About Us");
 		menuBar.add(mntmNewMenuItem_2);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Register");
 		menuBar.add(mntmNewMenuItem_1);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem = new JMenuItem("Login");
 		menuBar.add(mntmNewMenuItem);
 		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
 		
@@ -109,7 +109,7 @@ public class RegistrationView extends JFrame {
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Register");
+		JButton btnNewButton = new JButton("REGISTER");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -121,15 +121,14 @@ public class RegistrationView extends JFrame {
 		JLabel lblNewLabel_4 = new JLabel("REGISTER");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 50));
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
 		
-		JLabel lblNewLabel_5 = new JLabel("EMAIL");
+		JLabel lblNewLabel_5 = new JLabel("DATE OF BIRTH");
 		
 		JLabel lblNewLabel_6 = new JLabel("PASSWORD");
+		
+		JDateChooser dateChooser = new JDateChooser();
 		GroupLayout gl_panel_1_1 = new GroupLayout(panel_1_1);
 		gl_panel_1_1.setHorizontalGroup(
 			gl_panel_1_1.createParallelGroup(Alignment.TRAILING)
@@ -137,14 +136,11 @@ public class RegistrationView extends JFrame {
 					.addContainerGap(54, Short.MAX_VALUE)
 					.addGroup(gl_panel_1_1.createParallelGroup(Alignment.LEADING)
 						.addGroup(Alignment.TRAILING, gl_panel_1_1.createSequentialGroup()
-							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
-							.addGap(87))
-						.addGroup(Alignment.TRAILING, gl_panel_1_1.createSequentialGroup()
 							.addGroup(gl_panel_1_1.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblNewLabel_3)
-								.addGroup(gl_panel_1_1.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(textField_1, Alignment.TRAILING)
-									.addGroup(Alignment.TRAILING, gl_panel_1_1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_1_1.createParallelGroup(Alignment.TRAILING, false)
+									.addComponent(textField_1)
+									.addGroup(gl_panel_1_1.createParallelGroup(Alignment.LEADING)
 										.addComponent(lblNewLabel)
 										.addGroup(gl_panel_1_1.createParallelGroup(Alignment.TRAILING, false)
 											.addComponent(textField, Alignment.LEADING)
@@ -156,14 +152,17 @@ public class RegistrationView extends JFrame {
 														.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
 														.addGroup(gl_panel_1_1.createParallelGroup(Alignment.TRAILING, false)
 															.addComponent(textField_3, Alignment.LEADING)
-															.addComponent(lblNewLabel_5, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+															.addComponent(lblNewLabel_5, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+															.addComponent(dateChooser, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 															.addComponent(lblNewLabel_6, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
-															.addComponent(textField_5, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 296, Short.MAX_VALUE)
-															.addComponent(textField_4, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 296, Short.MAX_VALUE)))))))))
+															.addComponent(textField_5, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 296, Short.MAX_VALUE)))))))))
 							.addGap(41))
 						.addGroup(Alignment.TRAILING, gl_panel_1_1.createSequentialGroup()
 							.addComponent(lblNewLabel_4)
-							.addGap(78))))
+							.addGap(78))
+						.addGroup(Alignment.TRAILING, gl_panel_1_1.createSequentialGroup()
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
+							.addGap(109))))
 		);
 		gl_panel_1_1.setVerticalGroup(
 			gl_panel_1_1.createParallelGroup(Alignment.LEADING)
@@ -189,14 +188,14 @@ public class RegistrationView extends JFrame {
 					.addGap(30)
 					.addComponent(lblNewLabel_5)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
-					.addGap(33)
+					.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+					.addGap(27)
 					.addComponent(lblNewLabel_6)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
+					.addGap(44)
 					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
-					.addGap(21))
+					.addGap(56))
 		);
 		panel_1_1.setLayout(gl_panel_1_1);
 	}
